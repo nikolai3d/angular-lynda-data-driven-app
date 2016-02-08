ff22405e570c792a15d104a56f7f0e8cd0381bda
Nikolai Svakhin <svakhine@gmail.com>
2016-02-08 03:23:50 +0000

    Added meeting counter element

--- js/controllers/meetings.js
+++ js/controllers/meetings.js
@@ -10,7 +10,23 @@ gApp.controller('MeetingsController', ['$scope', '$rootScope', '$firebaseAuth', 
 
                 var meetingsInfo = $firebaseArray(meetingsDBRef); //We'll push meeting info into this
 
+                //This array is now synchronized, and will change as the records are added/deleted
+                //This is FireBase feature called "three-way data binding". Very cool.
+
                 $scope.ourMeetings = meetingsInfo;
+
+                var updateMeetingCount = function(){
+                    $rootScope.howManyMeetings = meetingsInfo.length;
+                }
+
+                var meetingsDataLoadedPromise = meetingsInfo.$loaded(); //promise which is 
+                    //fulfilled whenever data arrives from the database;
+                
+                meetingsDataLoadedPromise.then(updateMeetingCount); //promise handler 
+                    //This fires when data is loaded from the database.
+                
+                meetingsInfo.$watch(updateMeetingCount);
+                    //This fires when data is changed by the application.
 
                 $scope.addMeeting = function() { //Called from Add Meeting form's submit
                     var addMeetingPromise = meetingsInfo.$add({
