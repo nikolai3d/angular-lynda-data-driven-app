984b6e956a94ac69c5bb2ee67c29bb009447dd71
Nikolai Svakhin <svakhine@gmail.com>
2016-02-08 01:39:57 +0000

    Added a database insert after 'add meeting' is clicked

--- js/controllers/meetings.js
+++ js/controllers/meetings.js
@@ -1,3 +1,27 @@
-gApp.controller('MeetingsController', ['$scope', function($scope) {
-  $scope.message = "Meeting Controller Success!!!";
-}]);
+gApp.controller('MeetingsController', ['$scope', '$rootScope', '$firebaseAuth', '$firebaseArray', 'FIREBASE_URL',
+    function($scope, $rootScope, $firebaseAuth, $firebaseArray, FIREBASE_URL) {
+
+        var fbRef = new Firebase(FIREBASE_URL);
+        var fbAuth = $firebaseAuth(fbRef);
+
+        fbAuth.$onAuth(function(iAuthUserData) {
+            if (iAuthUserData) {
+                var meetingsDBRef = new Firebase(FIREBASE_URL + 'users/' + $rootScope.currentUser.$id + '/meetings');
+
+                var meetingsInfo = $firebaseArray(meetingsDBRef); //We'll push meeting info into this
+
+                $scope.addMeeting = function() { //Called from Add Meeting form's submit
+                    var addMeetingPromise = meetingsInfo.$add({
+                        name: $scope.meetingname,
+                        date: Firebase.ServerValue.TIMESTAMP
+                    }); //add started
+
+                    addMeetingPromise.then(function() {
+                        $scope.meetingname = '';
+                    }); //promise, add done
+
+                }; //addMeeting
+            }; //User authenticated
+        }); //$onAuth
+    } //Controller
+]);
