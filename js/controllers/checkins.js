007a66896e80560b3cfe7593bbd3e5c6fe49be6d
Nikolai Svakhin <svakhine@gmail.com>
2016-02-09 03:11:22 +0000

    Deleting check-ins now works.

--- js/controllers/checkins.js
+++ js/controllers/checkins.js
@@ -20,30 +20,41 @@ gApp.controller('CheckInsController', [
 
         $scope.whichMeeting = $routeParams.mId;
         $scope.whichUser = $routeParams.uId;
-        
-        var fbMeetingRef = new Firebase(FIREBASE_URL + 'users/' + $scope.whichUser + '/meetings/' + 
-        $scope.whichMeeting + '/checkins');
-        
+
+        var fbMeetingRef = new Firebase(FIREBASE_URL + 'users/' + $scope.whichUser + '/meetings/' +
+            $scope.whichMeeting + '/checkins');
+
         var checkinsList = $firebaseArray(fbMeetingRef);
-        
+
         $scope.currentCheckins = checkinsList; //Because of three-way data binding, as users are checking in, 
-                                               //the list will update on everybody's screens (checkinslist.html)
-        
-        
+        //the list will update on everybody's screens (checkinslist.html)
+
+
         $scope.addCheckin = function() {
             var checkinsInfo = $firebaseArray(fbMeetingRef);
             var checkinEntry = {
-                firstname: $scope.user.firstname, 
+                firstname: $scope.user.firstname,
                 lastname: $scope.user.lastname,
                 email: $scope.user.email,
                 date: Firebase.ServerValue.TIMESTAMP
             }; //checkinEntry
-            
+
             var checkinAddedPromise = checkinsInfo.$add(checkinEntry);
-            
-            checkinAddedPromise.then(function(){
-                $location.path('/checkins/'+$scope.whichUser+'/'+$scope.whichMeeting+'/checkinslist');
+
+            checkinAddedPromise.then(function() {
+                $location.path('/checkins/' + $scope.whichUser + '/' + $scope.whichMeeting + '/checkinslist');
             }); //on checkin add send the user to the checkins list. 
         }; //addCheckin
+
+        $scope.deleteCheckin = function(id) {
+            var fbCheckinRef = new Firebase(FIREBASE_URL + 
+            'users/' + $scope.whichUser + 
+            '/meetings/' + $scope.whichMeeting + 
+            '/checkins/' + id);
+
+            var fbRecord = $firebaseObject(fbCheckinRef);
+
+            fbRecord.$remove(id);
+        }; //deleteCheckin
     } //Controller
 ]);
